d28f41522b8a09104972257a4c3e0382c5842679
JangWheesung <gameplay1qaz@gmail.com>
2024-01-16 22:12:35 +0900

    인벤토리 아이템 및 연결

---
+++ Assets/02_Script/UI/Inventory/Inventory.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EnumList;
+
+public class Inventory : MonoBehaviour
+{
+    public event Action<GameObject> OnSlotClickEvt;
+
+    private SlotUI[] slots;
+
+    [HideInInspector] public bool isHold;
+    private int slotIdx;
+
+    public void HoldItem(GameObject itemObj, int idx)
+    {
+        isHold = true;
+        slotIdx = idx;
+
+        OnSlotClickEvt?.Invoke(itemObj); //기존에 들던 무기 pop하고 새로운 무기들기
+    }
+
+    public void Deleteltem() //아이템 소진시 준표가 갇고와서 발행
+    {
+        if (!isHold) return;
+        isHold = false;
+
+        slots[slotIdx].ResetSlot();
+    }
+}
---
+++ Assets/02_Script/UI/Inventory/SlotUI.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class SlotUI : MonoBehaviour, IPointerEnterHandler
+{
+    [HideInInspector] public SlotData slotData { get; set; }
+    [HideInInspector] public int slotIndex { get; set; } //이건 인벤토리에서 부여
+
+    private Inventory inventory;
+    private Image slotSprite;
+    private TextMeshProUGUI slotExplanation;
+
+    public void InsertSlot(SlotData newData)
+    {
+        slotData = newData;
+        slotSprite.sprite = slotData.slotSprite;
+    }
+
+    public void TouchSlot() //아이템 왼클릭 시(손에 들게하기)
+    {
+        if (slotData == null) return;
+
+        //풀링 만들기 전까진 Resources에서 가져오자
+        GameObject itemObj = Resources.Load<GameObject>($"ItemObj/{slotData.poolingName}");
+        inventory.HoldItem(itemObj, slotIndex);
+    }
+
+    public void ResetSlot() //아이템 소진시(슬롯 비우기)
+    {
+        slotData = null;
+
+        slotSprite.sprite = null;
+        slotExplanation.text = "";
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) //마우스포인터 닿으면 설명
+    {
+        if (slotData == null) return; //아님 텍스트 비우던지
+
+        slotExplanation.text = $"\"{slotData.slotExplanation}\"";
+    }
+}
