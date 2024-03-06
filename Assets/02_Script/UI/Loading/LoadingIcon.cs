5e3d6c0322f3ca8637c660456caba66ee64d2177
JangWheesung <gameplay1qaz@gmail.com>
2024-03-07 00:46:43 +0900

    맵 로딩UI 완료

---
+++ Assets/02_Script/UI/Loading/LoadingIcon.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LoadingIcon : MonoBehaviour
+{
+    [SerializeField] private RectTransform[] pieces;
+    [SerializeField] private float radius;
+
+    private List<RectTransform> rollPiece = new List<RectTransform>();
+    private Vector2 center;
+
+    private float[] value;
+
+    private void Start()
+    {
+        center = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        value = new float[pieces.Length];
+
+        StartCoroutine(PieceCor());
+    }
+
+    private void Update()
+    {
+        if (rollPiece.Count == 0) return;
+
+        for (int i = 0; i < rollPiece.Count; i++)
+        {
+            Vector2 rectTrs = new Vector2();
+            rectTrs.x = center.x + Mathf.Sin(value[i]) * radius;
+            rectTrs.y = center.y + Mathf.Cos(value[i]) * radius;
+
+            rollPiece[i].position = rectTrs;
+        }
+    }
+
+    private void CircleMove(int idx)
+    {
+        DOTween.To(() => value[idx], x => value[idx] = x, 6.4f, 1f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            value[idx] = 0f;
+            CircleMove(idx);
+        });
+    }
+
+    private IEnumerator PieceCor()
+    {
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            rollPiece.Add(pieces[i]);
+            CircleMove(i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
--- Assets/02_Script/UI/Profile/ProfileLayout.cs
+++ Assets/02_Script/UI/Profile/ProfileLayout.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class ProfileLayout : MonoBehaviour
 {
-    public GameObject centerPoint; // 중심점
     public float semiMajorAxis; // 타원의 장축 반지름
     public float semiMinorAxis; // 타원의 단축 반지름
 
+    int childCount;
 
-    public void ArrangeChildren()
+    private void Update()
     {
+        if (childCount != transform.childCount)
+        {
+            childCount = transform.childCount;
+            ArrangeChildren();
+        }
+    }
 
-        int childCount = transform.childCount;
-        if (childCount == 0) return;
-
+    private void ArrangeChildren() //유저 추가 시 호출
+    {
         float angleStep = 360f / childCount;
 
         for (int i = 0; i < childCount; i++)
         {
             float angle = i * angleStep + 90f;
-            float x = centerPoint.transform.position.x + semiMajorAxis * Mathf.Cos(angle * Mathf.Deg2Rad);
-            float y = centerPoint.transform.position.y + semiMinorAxis * Mathf.Sin(angle * Mathf.Deg2Rad);
+            float x = transform.position.x + semiMajorAxis * Mathf.Cos(angle * Mathf.Deg2Rad);
+            float y = transform.position.y + semiMinorAxis * Mathf.Sin(angle * Mathf.Deg2Rad);
 
             Transform child = transform.GetChild(i);
-            child.position = new Vector3(x, y, 0f);
+            child.position = new Vector3(x, y, 0);
         }
     }
 }
