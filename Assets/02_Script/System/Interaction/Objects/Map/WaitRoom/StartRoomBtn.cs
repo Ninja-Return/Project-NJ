f08e7d04f17058430abbc17dd6bc889d714a8a12
HY30K <jun44227@gmail.com>
2024-03-26 10:07:16 +0900

    시작하면 안내 패널 꺼지는거 만듬

--- Assets/02_Script/System/Interaction/Objects/Map/WaitRoom/StartRoomBtn.cs
+++ Assets/02_Script/System/Interaction/Objects/Map/WaitRoom/StartRoomBtn.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class StartRoomBtn : InteractionObject
 {
 
+    [SerializeField] private GameObject uiPanel1;
+    [SerializeField] private GameObject uiPanel2;
+
     protected override void DoInteraction()
     {
 
         if (!IsServer || WaitRoomManager.Instance.IsRunningGame.Value == true) return;
 
         WaitRoomManager.Instance.IsRunningGame.Value = true;
+
+        uiPanel1.SetActive(false);
+        uiPanel2.SetActive(false);
+
         NetworkManager.SceneManager.LoadScene("Room", LoadSceneMode.Additive);
 
     }
