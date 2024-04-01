f04056959a7451630968f8b3bf664b3498ac2a8c
JangWheesung <gameplay1qaz@gmail.com>
2024-04-01 18:17:05 +0900

    찐 수정

--- Assets/02_Script/System/Tutorial/TutorialSystem.cs
+++ Assets/02_Script/System/Tutorial/TutorialSystem.cs
@@ -143,6 +143,8 @@ public class TutorialSystem : NetworkBehaviour
         //player = Instantiate(playerController, startPos.position, Quaternion.identity);
         //player.NetworkObject.SpawnAsPlayerObject(OwnerClientId);
 
+        WaitRoomManager.Instance.IsRunningGame.Value = true;
+
     }
 
     private void Start()
