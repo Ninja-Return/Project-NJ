7a096f77333102247078e44ce2071da54103085b
HY30K <jun44227@gmail.com>
2024-04-17 19:27:29 +0900

    StartUI만들고 스크립팅까지는 함 이제 호출만 해주면됨

--- Assets/02_Script/HY30K/UI/StartGameText.cs
+++ Assets/02_Script/HY30K/UI/StartGameText.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 using TMPro;
 using System;
 using Unity.Netcode;
+using Michsky.UI.Dark;
 
 public class StartGameText : NetworkBehaviour
 {
-    [SerializeField] private GameObject startAlertPanel;
+    //[SerializeField] private GameObject startAlertPanel;
+    public UIDissolveEffect dissolveEffect;
+    public MainPanelManager mainPanelManager;
 
     private void Start()
     {
 
+        dissolveEffect.DissolveOut();
+
+    }
+
+    public void DOStart()
+    {
         if (IsServer)
         {
 
             New_GameManager.Instance.OnGameStarted += HandleGameStartedClientRpc;
 
         }
-
     }
 
     [ClientRpc]
@@ -27,18 +35,29 @@ public class StartGameText : NetworkBehaviour
 
         NetworkSoundManager.Play2DSound("GameStart");
         WaitRoomManager.Instance.UnActiveLoadingPanel();
-        StartCoroutine(StartAlert());
+        StartCoroutine(StartText());
 
     }
 
-    private IEnumerator StartAlert()
+    private IEnumerator StartText()
     {
 
-        startAlertPanel.transform.TVEffect(true);
+        mainPanelManager.OpenPanel("Alert");
 
         yield return new WaitForSeconds(3f);
 
-        startAlertPanel.transform.TVEffect(false);
+        mainPanelManager.OpenFirstTab();
 
     }
+
+    /* private IEnumerator StartAlert()
+     {
+
+         startAlertPanel.transform.TVEffect(true);
+
+         yield return new WaitForSeconds(3f);
+
+         startAlertPanel.transform.TVEffect(false);
+
+     }*/
 }
