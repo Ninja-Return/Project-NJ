f9519399352cf1f57241d3aa3cdf4ce390f4cf51
준1 <98933515+Wizone06@users.noreply.github.com>
2024-04-23 12:39:49 +0900

    탐지기 완성
    탐지기 완성

---
+++ Assets/02_Script/Wonjun/Sensor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Sensor : HandItemRoot
+{
+    [SerializeField] private float sensorRange;
+    public LayerMask EnemyLayer;
+
+    [SerializeField] private NetworkObject SensorPrefab;
+    [SerializeField] private float firePower;
+    [SerializeField] private float spawnDistance = .5f; // 앞으로 스폰할 거리
+    [SerializeField] private bool OnSensor; 
+
+    public override void DoUse()
+    {
+        Vector3 forwardDirection = Camera.main.transform.forward;
+
+        Vector3 spawnPosition = transform.position + forwardDirection * spawnDistance;
+        NetworkObject newSensor = Instantiate(SensorPrefab, spawnPosition, Quaternion.identity);
+        newSensor.Spawn(true);
+
+
+        Rigidbody seosorRigid = newSensor.GetComponent<Rigidbody>();
+        seosorRigid.AddForce(forwardDirection * firePower, ForceMode.Impulse);
+    }
+
+    private void Start()
+    {
+        OnSensor = false;
+    }
+
+    private void Update()
+    {
+
+        Collider[] detectedColliders = Physics.OverlapSphere(transform.position, sensorRange, EnemyLayer);
+
+        if (detectedColliders.Length > 0 && !OnSensor)
+        {
+            NetworkSoundManager.Play3DSound("Sensor", transform.position, 0.1f, 30f, SoundType.SFX, AudioRolloffMode.Linear);
+            Debug.Log("소리");
+            OnSensor = true;
+        }
+
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(this.transform.position, sensorRange);
+    }
+}
