499d1b2a9764bb43155fda333668ffdd7d7bbbab
dkdkdsa <bos94330@gmail.com>
2024-04-30 11:39:39 +0900

    소다와 덤밸

--- Assets/02_Script/Item/HandItem/Soda.cs
+++ Assets/02_Script/Item/HandItem/Soda.cs
@@ -1,14 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Soda : HandItemRoot
 {
+
+    private PlayerAnimationController animator;
+    private PlayerEventSystem eventSystem;
+    private bool played;
+
+    private void Start()
+    {
+
+        if (!isOwner) return;
+
+        var player = PlayerManager.Instance.localController;
+
+        animator = player.GetComponent<PlayerAnimationController>();
+        eventSystem = player.GetComponent<PlayerEventSystem>();
+        eventSystem.OnTweenAnimeEvent += HandleAnimeEnd;
+
+    }
+
+    private void HandleAnimeEnd()
+    {
+
+        PlayerManager.Instance.localController.AddSpeed(3, 3);
+        Inventory.Instance.Deleteltem();
+
+    }
+
     public override void DoUse()
     {
 
+        if(played) return;
+        played = true;
+
+        animator.PlayTweenAnimation("Soda");
+
         NetworkSoundManager.Play3DSound("SodaOpen", transform.position, 0.01f, 15f);
-        PlayerManager.Instance.localController.AddSpeed(3, 3);
 
     }
 
--- Assets/02_Script/System/Monster/MonsterSpawnSystem.cs
+++ Assets/02_Script/System/Monster/MonsterSpawnSystem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
-using static UnityEditor.Rendering.ShadowCascadeGUI;
+
 
 public class MonsterSpawnSystem : NetworkBehaviour
 {
