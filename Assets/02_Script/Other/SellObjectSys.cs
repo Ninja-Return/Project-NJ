fee91b8ee472d145bdf5e45611c88a3808f22999
dkdkdsa <bos94330@gmail.com>
2024-05-01 00:34:25 +0900

    상점 열리는 버그 수정

--- Assets/02_Script/Other/SellObjectSys.cs
+++ Assets/02_Script/Other/SellObjectSys.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class SellObjectSys : InteractionObject
@@ -9,29 +10,63 @@ public class SellObjectSys : InteractionObject
     [SerializeField] private Transform moveObject;
     [SerializeField] private SellSystem sellSystem;
 
+    private NetworkVariable<bool> locked;
+
+    public override void OnNetworkSpawn()
+    {
+
+        locked = new();
+
+    }
+
     protected override void DoInteraction()
     {
 
+        if (locked.Value) return;
+
         NetworkSoundManager.Play3DSound("BoxOpen", transform.position, 0.1f, 10f);
+
+        MoveServerRPC(NetworkManager.LocalClientId);
+
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void MoveServerRPC(ulong buyClientId)
+    {
+
+        locked.Value = true;
 
         Sequence seq = DOTween.Sequence();
         seq.Append(moveObject.DOLocalMoveZ(-0.6f, 0.3f).SetEase(Ease.OutQuad));
         seq.AppendInterval(1);
-        seq.AppendCallback(Selling);
+        seq.AppendCallback(() => Selling(buyClientId));
         seq.AppendInterval(1)
-            .OnComplete(() => 
+            .OnComplete(() =>
             {
                 NetworkSoundManager.Play3DSound("BoxClose", transform.position, 0.1f, 10f);
             });
         seq.Append(moveObject.DOLocalMoveZ(0.47f, 0.3f).SetEase(Ease.OutQuad));
+        seq.AppendCallback(() =>
+        {
+
+            locked.Value = false;
+
+        });
 
     }
 
-    private void Selling()
+    private void Selling(ulong buyClientId)
     {
 
         NetworkSoundManager.Play3DSound("Sell", transform.position, 0.01f, 10f);
-        sellSystem.Sell(NetworkManager.LocalClientId);
+        sellSystem.Sell(buyClientId);
+
+    }
+
+    public override void OnNetworkDespawn()
+    {
+
+        locked.Dispose();
 
     }
 
