c8deae6915f6daf1b10451869400308d5b87d47b
dkdkdsa <bos94330@gmail.com>
2024-05-08 14:05:31 +0900

    ㅁㄴㅇ

--- Assets/02_Script/System/Map/MapGenerater.cs
+++ Assets/02_Script/System/Map/MapGenerater.cs
@@ -81,7 +81,7 @@ public class MapGenerater : NetworkBehaviour
 
     }
 
-    [ServerRpc(RequireOwnership = false¤º)]
+    [ServerRpc(RequireOwnership = false)]
     private void JoinServerRPC()
     {
 
