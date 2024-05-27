81cbacb5031109765180c07286edc94557cfb92c
준1 <98933515+Wizone06@users.noreply.github.com>
2024-05-28 00:35:41 +0900

    홀로그램 시스템 완성(아직 홀로그램 쉐이더는 안넣음)
    홀로그램 시스템 완성(아직 홀로그램 쉐이더는 안넣음)(1교시에 홀로그램 쉐이더 넣기)

--- Assets/02_Script/Wonjun/MSpawnItem.cs
+++ Assets/02_Script/Wonjun/MSpawnItem.cs
@@ -6,18 +6,53 @@ using UnityEngine;
 public class MSpawnItem : HandItemRoot
 {
     [SerializeField] private NetworkObject monsterPrefab;
+    [SerializeField] private GameObject spawnIndicatorPrefab;
+    [SerializeField] private float spawnDistance = 2f;
+    private GameObject currentSpawnIndicator;
+    Vector3 spawnMonsterPosition;
+    Transform playerTransform;
+
     private void Start()
     {
+        currentSpawnIndicator = Instantiate(spawnIndicatorPrefab);
+        currentSpawnIndicator.SetActive(true);
     }
 
     public override void DoUse()
     {
+        playerTransform = PlayerManager.Instance.localController.transform;
+        Vector3 spawnPosition = playerTransform.position + playerTransform.forward * spawnDistance;
 
-        var trmPos = PlayerManager.Instance.localController.transform.position;
+        RaycastHit hit;
+        if (Physics.Raycast(playerTransform.position, playerTransform.forward, out hit, spawnDistance))
+        {
+            spawnPosition = ((playerTransform.position - hit.point).normalized * .5f) + hit.point; 
+        }
 
-        MonsterSpawnSystem.Instance.SpawnMonsterServerRPC(trmPos, 2f);
+        currentSpawnIndicator.SetActive(false);
+
+        MonsterSpawnSystem.Instance.SpawnMonsterServerRPC(spawnPosition, 2f);
     }
 
-    
+    private void Update()
+    {
+        playerTransform = PlayerManager.Instance.localController.transform;
 
+        spawnMonsterPosition = playerTransform.position + playerTransform.forward * spawnDistance;
+
+        RaycastHit hit;
+        if (Physics.Raycast(playerTransform.position, playerTransform.forward, out hit, spawnDistance))
+        {
+            spawnMonsterPosition = ((playerTransform.position - hit.point).normalized * .5f) + hit.point;
+
+        }
+
+
+        currentSpawnIndicator.transform.position = spawnMonsterPosition - new Vector3(0f, 1f,0f);
+    }
+
+    public void OnDisable()
+    {
+        currentSpawnIndicator.SetActive(false);
+    }
 }
