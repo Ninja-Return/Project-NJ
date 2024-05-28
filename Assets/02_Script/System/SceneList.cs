846430500882d12dd7298f3d9cd93ee5463a4394
JangWheesung <gameplay1qaz@gmail.com>
2024-05-28 20:21:02 +0900

    시작방 리메이크(게임 진행방법 설명)

--- Assets/02_Script/System/SceneList.cs
+++ Assets/02_Script/System/SceneList.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class SceneList
 {
 
     public const string TutorialScene = "Tutorial"; //Wheesong, TestDron, Tutorial
     public const string SingleGameScene = "Office_Map_Single";
-    public const string OfficeMap = "TestDron"; //Play
+    public const string OfficeMap = "Play"; //Play
     public const string TunnelMap = "Tunnel";
     public const string LobbyScene = "Lobby"; //Lobby
     public const string LobbySelectScene = "LobbySelect";
